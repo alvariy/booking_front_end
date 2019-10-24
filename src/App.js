35099a024dbcc87f25aa3c3aa5cd22629f2a7598
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-24 11:29:25 +0800

    Add Package Ware House UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PickUpInput from './components/PickUpInput'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import PackageWarehouseInput from './components/PackageWarehouseInput';
 
 function App() {
   return (
     <div className="App">
-    <PickUpInput/>
+        <Router>
+        <div>
+            <Link to="/setPickUpDate">Set Pick Up Date</Link>
+            <span>/</span>
+            <Link to="/homePage">Home</Link>
+            </div>
+
+            <Switch>
+          <Route exact path="/setPickUpDate">
+          <PickUpInput/>
+          </Route>
+          <Route path="/homePage">
+          <PackageWarehouseInput/>
+          </Route>
+        </Switch>
+
+        </Router>
+    </div>
+  );
+}
+function Home() {
+  return (
+    <div >
+      <h1> Home</h1>
     </div>
   );
 }
---
+++ src/components/PackageWarehouseInput.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+class PackageWarehouseInput extends React.Component{
+    constructor(props){
+        super(props);
+            this.state = {
+                wayBillNumber: "",
+                recipients: "",
+                phone: "",
+                weight: ""
+            }
+        }
+
+        handlewayBillNumber = (event) => {
+            this.setState({wayBillNumber: event.target.value});
+            console.log(this.state.wayBillNumber);
+           };
+    
+           handlereceipients = (event) => {
+            this.setState({recipients: event.target.value});
+            console.log(this.state.recipients);
+           };
+
+           handlePhone = (event) => {
+            this.setState({phone: event.target.value});
+            console.log(this.state.phone);
+           };
+
+           handleWeight = (event) => {
+            this.setState({weight: event.target.value});
+            console.log(this.state.weight);
+           };
+
+        
+        addPackage = () => {
+            console.log(this.state);
+            fetch("http://localhost:8080/", {mode: 'cors' , 
+             method: 'POST',
+             headers: new Headers({'Content-Type': 'application/json'}),
+             body: JSON.stringify(this.state)
+            },).then(res => res.json());
+        }
+
+    render(){
+        return(
+            <div>
+                 <div>
+                     <div>
+            <span>Waybill Number:</span>
+            <input type="text" value = {this.state.wayBillNumber} onChange={this.handlewayBillNumber}/>
+            </div>
+            <div>
+            <span>Receipients :</span>
+            <input type="text"  value = {this.state.recipients} onChange={this.handlereceipients}/>
+            </div>
+            <div>
+            <span>Phone:</span>
+            <input type="text"  value = {this.state.phone} onChange={this.handlePhone}/>
+            </div>
+            <div>
+            <span>Weight:</span>
+            <input type="text"  value = {this.state.weight} onChange={this.handleWeight}/>
+            </div>
+           </div>
+           <button onClick={this.addPackage}>Confirm</button>
+           <button onClick={this.addTask}>Cancel</button>
+            </div>
+        )
+    }
+
+}
+
+export default PackageWarehouseInput
